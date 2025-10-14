ce1be52de4cffcc36d140ce09230ae820d555620
ro374-dot <ro374@nau.edu>
2025-10-14 08:39:41 -0700

    Update app.js

--- homework/app.js
+++ homework/app.js
@@ -1,1 +1,14 @@
 console.log('Hello World!');
+
+let hasDownloadedResume = false;
+
+document.getElementById('downloadBtn').addEventListener('click', () => {
+  if (!hasDownloadedResume) {
+    setTimeout(() => {
+      alert("Your resume is downloaded successfully!");
+    }, 2000);
+    hasDownloadedResume = true;
+  }
+});
+const ownerName = "Rudy"; // or your name
+let hasDownloadedResume = false; // already declared above
